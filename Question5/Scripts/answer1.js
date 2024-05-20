a63e7573804d7bd6d188f4c739c82d93ea3557f1
pc-aide <36453201+pc-aide@users.noreply.github.com>
2024-05-20 00:51:51 -0400

    Update answer1.js

--- Question5/Scripts/answer1.js
+++ Question5/Scripts/answer1.js
@@ -1,1 +1,28 @@
+function checkAnswer1(correctAnswer, solutionInfoId, solutionText) {
+    let isCorrect = true;
+    const select1 = document.querySelector('#select1Option1').value;
+    const select2 = document.querySelector('#select1Option2').value;
+    const userAnswer = [select1, select2];
 
+    userAnswer.forEach((answer, index) => {
+        if (answer !== correctAnswer[index]) {
+            isCorrect = false;
+        }
+    });
+
+    const solutionInfo = document.getElementById(solutionInfoId);
+    if (isCorrect) {
+        solutionInfo.innerHTML = solutionText;
+        solutionInfo.classList.remove('incorrect');
+        solutionInfo.classList.add('highlight');
+        score += 1; // Ajouter un point si la réponse est correcte
+    } else {
+        const userAnswerText = userAnswer.length ? userAnswer.join(', ') : "non défini";
+        solutionInfo.innerHTML = `Your answer: ${userAnswerText}.<br><br>Correct answer: ${correctAnswer.join(', ')}.<br><br>Explanation: 'Boot diagnostics must be enabled for the VM' and 'The Azure account accessing Serial Console must have Virtual Machine Contributor role for both the VM and the boot diagnostics storage account'.<br><br>For reference: <a href='https://docs.microsoft.com/en-us/troubleshoot/azure/virtual-machines/serial-console-overview' target='_blank'>Serial Console overview</a>.`;
+        solutionInfo.classList.remove('highlight');
+        solutionInfo.classList.add('incorrect');
+    }
+
+    solutionInfo.style.display = 'block';
+    showFinalScore();
+}
