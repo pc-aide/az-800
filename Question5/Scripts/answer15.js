446a38ddf42ab372c70b0a29f882fb2dee3581ab
pc-aide <36453201+pc-aide@users.noreply.github.com>
2024-05-21 04:38:44 -0400

    Create answer15.js

---
+++ Question5/Scripts/answer15.js
@@ -0,0 +1,45 @@
+function attachSolutionButtonListeners_answer15(button) {
+    button.addEventListener('click', function() {
+        const solutionInfoId = this.dataset.solutionInfoId;
+        const correctAnswer = this.dataset.correctAnswer.split(',');
+        checkAnswer15(correctAnswer, solutionInfoId, this.dataset.solutionText);
+    });
+}
+
+function checkAnswer15(correctAnswer, solutionInfoId, solutionText) {
+    const selectCommand = document.querySelector('#selectCommand').value;
+    const selectSSH = document.querySelector('#selectSSH').value;
+    const userAnswer = [selectCommand, selectSSH];
+
+    const isCorrect = (userAnswer[0] === correctAnswer[0] && userAnswer[1] === correctAnswer[1]);
+
+    const solutionInfo = document.getElementById(solutionInfoId);
+    if (isCorrect) {
+        solutionInfo.innerHTML = solutionText;
+        solutionInfo.classList.remove('incorrect');
+        solutionInfo.classList.add('highlight');
+        score += 1; // Ajouter un point si la réponse est correcte
+    } else {
+        const userAnswerText = `Your answer: Command: ${selectCommand}, SSH: ${selectSSH}.<br><br>Correct answer: Command: ${correctAnswer[0]}, SSH: ${correctAnswer[1]}`;
+        solutionInfo.innerHTML = `${userAnswerText}.<br><br>${solutionText}`;
+        solutionInfo.classList.remove('highlight');
+        solutionInfo.classList.add('incorrect');
+    }
+
+    solutionInfo.style.display = 'block';
+    showFinalScore();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.solutionButton').forEach(button => {
+        attachSolutionButtonListeners_answer15(button);
+    });
+});
+
+function showFinalScore() {
+    const finalScoreElement = document.getElementById('finalScore');
+    if (finalScoreElement) {
+        const percentage = (score / totalQuestions) * 100;
+        finalScoreElement.textContent = `Final Score: ${score}/${totalQuestions} (${percentage.toFixed(2)}%)`;
+    }
+}
