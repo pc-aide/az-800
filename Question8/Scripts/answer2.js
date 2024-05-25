779c78bf6592eb539aef08b1b8cf3a10724b8d68
pc-aide <36453201+pc-aide@users.noreply.github.com>
2024-05-25 00:40:38 -0400

    Update answer2.js

---
+++ Question8/Scripts/answer2.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questionnaire</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="questionContainer"></div>
+    <div id="finalScore"></div>
+    <script src="Scripts/constants.js"></script>
+</body>
+</html>
