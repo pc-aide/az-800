c7704ba3d2a80b65ecc09f5b4c957544c66e1ef5
pc-aide <36453201+pc-aide@users.noreply.github.com>
2024-05-24 20:49:40 -0400

    Update answer1.js

--- Question8/Scripts/answer1.js
+++ Question8/Scripts/answer1.js
@@ -1,48 +1,49 @@
 function attachSolutionButtonListeners_answer1(button) {
-    button.addEventListener('click', function() {
-        const solutionInfoId = this.dataset.solutionInfoId;
-        const correctAnswer = this.dataset.correctAnswer.split(',');
-        checkAnswer1(correctAnswer, solutionInfoId, this.dataset.solutionText);
-    });
+  button.addEventListener('click', function() {
+      const solutionInfoId = this.dataset.solutionInfoId;
+      const correctAnswer = this.dataset.correctAnswer.split(',');
+      checkAnswer1(correctAnswer, solutionInfoId, this.dataset.solutionText);
+  });
 }
 
 function checkAnswer1(correctAnswer, solutionInfoId, solutionText) {
-    const user1Answer = document.querySelector('input[name="user1"]:checked') ? document.querySelector('input[name="user1"]:checked').value : '';
-    const user3Answer = document.querySelector('input[name="user3"]:checked') ? document.querySelector('input[name="user3"]:checked').value : '';
-    const user2Answer = document.querySelector('input[name="user2"]:checked') ? document.querySelector('input[name="user2"]:checked').value : '';
+  const userAnswers = [
+      document.querySelector('input[name="user1Read"]:checked') ? document.querySelector('input[name="user1Read"]:checked').value : '',
+      document.querySelector('input[name="user3Delete"]:checked') ? document.querySelector('input[name="user3Delete"]:checked').value : '',
+      document.querySelector('input[name="user2List"]:checked') ? document.querySelector('input[name="user2List"]:checked').value : ''
+  ];
 
-    const userAnswers = [user1Answer, user3Answer, user2Answer];
-    const isCorrect = userAnswers.length === correctAnswer.length && userAnswers.every((val, index) => val === correctAnswer[index]);
+  const isCorrect = userAnswers.length === correctAnswer.length && userAnswers.every((val, index) => val === correctAnswer[index]);
 
-    const solutionInfo = document.getElementById(solutionInfoId);
-    if (isCorrect) {
-        solutionInfo.innerHTML = solutionText;
-        solutionInfo.classList.remove('incorrect');
-        solutionInfo.classList.add('highlight');
-        score += 1; // Ajouter un point si la réponse est correcte
-    } else {
-        const userAnswersText = `Your answers: User1 can read the files in share1: ${user1Answer}, User3 can delete files in Share1: ${user3Answer}, If User2 connects to \\Server1.adatum.com from File Explorer, Share1 will be listed: ${user2Answer}.<br>`;
-        const correctAnswersText = `Correct answers: User1 can read the files in share1: Yes. User3 can delete files in Share1: No. If User2 connects to \\Server1.adatum.com from File Explorer, Share1 will be listed: No.<br>`;
-        const explanationText = `Explanation: Very important are share permissions. User1 has access share and permissions. User2 has only access on file. User3 has access to to share but not permission on folder.<br><br>Ref: <a href='https://learn.microsoft.com/en-us/azure/storage/files/storage-files-identity-ad-ds-configure-permissions' target='_blank'>Microsoft Learn</a>`;
-        solutionInfo.innerHTML = `${userAnswersText}<br>${correctAnswersText}<br>${explanationText}`;
-        solutionInfo.classList.remove('highlight');
-        solutionInfo.classList.add('incorrect');
-    }
+  const solutionInfo = document.getElementById(solutionInfoId);
+  if (isCorrect) {
+      solutionInfo.innerHTML = solutionText;
+      solutionInfo.classList.remove('incorrect');
+      solutionInfo.classList.add('highlight');
+      score += 1; // Ajouter un point si la réponse est correcte
+  } else {
+      const userAnswersText = `Your answers: User1 can read the files in share1: ${userAnswers[0]}, User3 can delete files in Share1: ${userAnswers[1]}, If User2 connects to \\\\Server1.adatum.com from File Explorer, Share1 will be listed: ${userAnswers[2]}.<br>`;
+      const correctAnswersText = `Correct answers: User1 can read the files in share1: ${correctAnswer[0]}, User3 can delete files in Share1: ${correctAnswer[1]}, If User2 connects to \\\\Server1.adatum.com from File Explorer, Share1 will be listed: ${correctAnswer[2]}.<br>`;
+      const explanationText = `Explanation: Box 1: Yes - Group1 has Read access to Folder1 and Change access to Share1. Therefore, User1 can read the files in Share1. Box 2: No - Group3 has Full Control access to Share1. However, Group3 has no permissions configured Folder1. Therefore, User3 cannot access the files in Share1. Group2 has write permission to Folder1. However, Group2 has no permission on Share1. Therefore, users in Group2 cannot access files in the shared folder. Access Based Enumeration when enabled hides files and folders that users do not have permission to access. However, Access Based Enumeration is not enabled on Share1. This is indicated by the FolderEnumerationMode ג€&quot; Unrestricted setting. Therefore, the share will be visible to User2 even though User2 cannot access the shared folder.<br><br>Ref: <a href='https://learn.microsoft.com/en-us/azure/storage/files/storage-files-identity-ad-ds-configure-permissions' target='_blank'>Microsoft Documentation</a>.`;
+      solutionInfo.innerHTML = `${userAnswersText}<br>${correctAnswersText}<br>${explanationText}`;
+      solutionInfo.classList.remove('highlight');
+      solutionInfo.classList.add('incorrect');
+  }
 
-    solutionInfo.style.display = 'block';
-    showFinalScore();
+  solutionInfo.style.display = 'block';
+  showFinalScore();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.solutionButton').forEach(button => {
-        attachSolutionButtonListeners_answer1(button);
-    });
+  document.querySelectorAll('.solutionButton').forEach(button => {
+      attachSolutionButtonListeners_answer1(button);
+  });
 });
 
 function showFinalScore() {
-    const finalScoreElement = document.getElementById('finalScore');
-    if (finalScoreElement) {
-        const percentage = (score / totalQuestions) * 100;
-        finalScoreElement.textContent = `Final Score: ${score}/${totalQuestions} (${percentage.toFixed(2)}%)`;
-    }
+  const finalScoreElement = document.getElementById('finalScore');
+  if (finalScoreElement) {
+      const percentage = (score / totalQuestions) * 100;
+      finalScoreElement.textContent = `Final Score: ${score}/${totalQuestions} (${percentage.toFixed(2)}%)`;
+  }
 }
