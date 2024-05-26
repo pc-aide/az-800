6dfde97080ee90d4ed52883d9fb5509db0cab394
pc-aide <36453201+pc-aide@users.noreply.github.com>
2024-05-25 23:10:16 -0400

    Update answer5.js

---
+++ Question9/Scripts/answer5.js
@@ -0,0 +1,28 @@
+function attachSolutionButtonListeners_question5(button) {
+    button.addEventListener('click', function() {
+        // Réinitialiser les couleurs des réponses
+        document.querySelectorAll('.question-row').forEach(row => {
+            row.classList.remove('incorrect', 'highlight');
+        });
+
+        // Vérifier la réponse
+        let allCorrect = true;
+
+        const selectedAnswer = document.querySelector('input[name="fsrm-install"]:checked');
+        if (selectedAnswer && selectedAnswer.value === 'C') {
+            document.querySelector('input[name="fsrm-install"][value="C"]').parentElement.classList.add('highlight');
+        } else {
+            document.querySelector('input[name="fsrm-install"][value="C"]').parentElement.classList.add('incorrect');
+            allCorrect = false;
+        }
+
+        // Mise à jour du score
+        if (allCorrect) {
+            score++;
+        }
+        showFinalScore();
+
+        // Afficher l'explication
+        document.getElementById('solutionInfo_question5').style.display = 'block';
+    });
+}
